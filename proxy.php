e7e29272d5aa5c5daac7a9245300e8a01fe6036a
irfanindo <48234690+irfanindo@users.noreply.github.com>
2024-05-12 20:27:12 +0700

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,11 +1,16 @@
 <?php
-// Ambil URL dari parameter GET
+// Ambil URL .m3u8 dari parameter query
 $url = $_GET['url'];
 
-// Set header agar konten dapat dimuat dalam iframe
-header("Content-Security-Policy: frame-ancestors 'self'");
-header("X-Frame-Options: allowall");
-
-// Buka URL eksternal dan kirimkan kembali konten
-echo file_get_contents($url);
+// Validasi URL
+if (filter_var($url, FILTER_VALIDATE_URL)) {
+    // Set header agar dapat menyimpan konten dan mengirim ulang header
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/vnd.apple.mpegurl');
+    
+    // Ambil konten dari URL .m3u8 dan kirimkan
+    echo file_get_contents($url);
+} else {
+    echo "URL tidak valid.";
+}
 ?>
